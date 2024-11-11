273a639585b576e5da5646926bee747f4087e84f
Ignacio Mena <ignacio.mena@manomano.com>
2024-11-11 16:59:32 +0100

    Session 8: Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddCandidate from './components/AddCandidateForm';
+import InterviewProcess from './components/InterviewProcess/InterviewProcess';
+import Positions from './components/Positions';
 import RecruiterDashboard from './components/RecruiterDashboard';
-import AddCandidate from './components/AddCandidateForm'; 
-import Positions from './components/Positions'; 
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<RecruiterDashboard />} />
         <Route path="/add-candidate" element={<AddCandidate />} /> {/* Agrega esta línea */}
         <Route path="/positions" element={<Positions />} />
+        <Route path="/positions/:id" element={<InterviewProcess />} />
       </Routes>
     </BrowserRouter>
   );
